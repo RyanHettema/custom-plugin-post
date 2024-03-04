e7f03d65feb628437bcc5030cfba8af155e808e8
RyanHettema <101569975+RyanHettema@users.noreply.github.com>
2024-03-04 15:49:47 +0100

    Refs:#7957 - Header plugin

---
+++ custom-post-plugin.php
@@ -0,0 +1,7 @@
+<?php
+/*
+Plugin Name: Custom Post Complete
+Description: Een plugin die een 'Persoon' post type toevoegt met de mogelijkheid om naam, adres, postcode, email, afbeelding en excerpt in te vullen en op te slaan in de database. Bevat ook een instellingenpagina via een shortcode.
+Version: 1.0
+Author: Ryan Hettema
+*/
